4b110507830aca217593451d0694f5a640d634e4
l0lawrence <llawrence@microsoft.com>
2022-11-21 15:35:41 -0800

    remove close() on cancelledError

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/aio/_transport_async.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/aio/_transport_async.py
@@ -557,7 +557,7 @@ class WebSocketTransportAsync(
                     n = 0
             return view
         except asyncio.CancelledError as ce:
-            await self.session.close()
+            # await self.session.close()
             #raise
             raise ConnectionError('Websocket connection closed: %r' % ce) from ce
         except asyncio.TimeoutError as te:
@@ -582,7 +582,7 @@ class WebSocketTransportAsync(
             try:
                 await self.ws.send_bytes(s)
             except asyncio.CancelledError as ce:
-                await self.session.close()
+                # await self.session.close()
                 raise ConnectionError('Websocket connection closed: %r' % ce) from ce
             except asyncio.TimeoutError as te:
                 raise ConnectionError('Send timed out (%s)' % te)
